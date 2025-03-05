4583272d415f9a2c6401878c935d2bd0f29a9fa8
Jyri Sarha <jyri.sarha@linux.intel.com>
2025-03-05 11:38:12 +0200

    Revert "src: mark code as "cold""
    This reverts commit 3d78863d8055a0a89624a9e789307b01800e1df7.

--- src/audio/src/src_ipc4.c
+++ src/audio/src/src_ipc4.c
@@ -76,7 +76,7 @@ int src_stream_pcm_sink_rate_check(struct ipc4_config_src cfg,
  * set up param then verify param. BTW for IPC3 path, the param is sent by
  * host driver.
  */
-__cold int src_set_params(struct processing_module *mod, struct sof_sink *sink)
+int src_set_params(struct processing_module *mod, struct sof_sink *sink)
 {
 	struct sof_ipc_stream_params src_params;
 	struct sof_ipc_stream_params *params = mod->stream_params;
@@ -116,8 +116,8 @@ __cold int src_set_params(struct processing_module *mod, struct sof_sink *sink)
 	return ret;
 }
 
-__cold void src_get_source_sink_params(struct comp_dev *dev, struct sof_source *source,
-				       struct sof_sink *sink)
+void src_get_source_sink_params(struct comp_dev *dev, struct sof_source *source,
+				struct sof_sink *sink)
 {
 	struct processing_module *mod = comp_mod(dev);
 	struct comp_data *cd = module_get_private_data(mod);
@@ -135,9 +135,9 @@ __cold void src_get_source_sink_params(struct comp_dev *dev, struct sof_source *
 	sink_set_rate(sink, cd->ipc_config.sink_rate);
 }
 
-__cold int src_prepare_general(struct processing_module *mod,
-			       struct sof_source *source,
-			       struct sof_sink *sink)
+int src_prepare_general(struct processing_module *mod,
+			struct sof_source *source,
+			struct sof_sink *sink)
 {
 	struct comp_data *cd = module_get_private_data(mod);
 	struct comp_dev *dev = mod->dev;

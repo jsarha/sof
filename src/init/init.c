548640a197ce86ed8e25ccce29e6484bba9bde4a
Jyri Sarha <jyri.sarha@linux.intel.com>
2025-03-05 18:30:26 +0200

    Revert "src/init/init.c: Call dummy_cold_function() in primary_core_init()"
    This reverts commit de811e06f0b167583b890872dea6435938d008d3.

--- src/init/init.c
+++ src/init/init.c
@@ -44,7 +44,6 @@
 #include <version.h>
 #endif
 #include <sof/lib/ams.h>
-#include <ipc4/base-config.h>
 
 LOG_MODULE_REGISTER(init, CONFIG_SOF_LOG_LEVEL);
 
@@ -335,7 +334,6 @@ static int primary_core_init(int argc, char *argv[], struct sof *sof)
 	lp_sram_unpack();
 #endif
 
-	dummy_cold_function(1);
 	/* should not return, except with Zephyr */
 	return task_main_start(sof);
 }
